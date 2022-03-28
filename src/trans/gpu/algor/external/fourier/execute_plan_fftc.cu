ec6a5522e6a18ab691600d673e8f4d294fd46801
Daan Degrauwe <daan.degrauwe@meteo.be>
2022-03-28 16:36:34 +0200

    generalized create_plan; created inline cufft transform

--- src/trans/gpu/algor/external/fourier/execute_plan_fftc.cu
+++ src/trans/gpu/algor/external/fourier/execute_plan_fftc.cu
@@ -98,3 +98,52 @@ else {
 
 }
 
+
+
+extern "C"
+void
+#ifdef TRANS_SINGLE
+execute_plan_fftc_inplace_ (cufftHandle * PLANp, int * ISIGNp, cufftComplex * data)
+#else
+execute_plan_fftc_inplace_ (cufftHandle * PLANp, int * ISIGNp, cufftDoubleComplex * data)
+#endif
+{
+  cufftHandle plan = *PLANp;
+  int ISIGN = *ISIGNp;
+  
+/*if (cudaDeviceSynchronize() != cudaSuccess){
+  	fprintf(stderr, "Cuda error: Failed to synchronize\n");
+  	return;	
+}*/
+  
+  if (ISIGN== -1)
+    {
+#ifdef TRANS_SINGLE
+    cufftSafeCall(cufftExecR2C(plan, (cufftReal*)data, data));
+#else
+    cufftSafeCall(cufftExecD2Z(plan, (cufftDoubleReal*)data, data));
+#endif
+    }
+  else if (ISIGN== 1)
+    {
+#ifdef TRANS_SINGLE
+    cufftSafeCall(cufftExecC2R(plan, data, (cufftReal*)data));
+#else
+    cufftSafeCall(cufftExecZ2D(plan, data, (cufftDoubleReal*)data));
+#endif
+    }
+  else 
+    {
+      abort();
+    }
+
+// cudaDeviceSynchronize();
+
+//if (cudaDeviceSynchronize() != cudaSuccess){
+//	fprintf(stderr, "Cuda error: Failed to synchronize\n");
+//	return;	
+//}
+
+
+}
+
