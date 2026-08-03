bd3a924a0eda5247ced5a60a36daf23bb80b1f86
Denis Haumont <76622294+dhaumont@users.noreply.github.com>
2026-08-03 16:17:16 +0200

    introductions of views

---
+++ src/trans/cpu/internal/inv_trans_view_ctl_mod.F90
@@ -0,0 +1,166 @@
+! (C) Copyright 2001- ECMWF.
+! (C) Copyright 2001- Meteo-France.
+! 
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+!
+
+MODULE INV_TRANS_VIEW_CTL_MOD
+CONTAINS
+SUBROUTINE INV_TRANS_VIEW_CTL(KF_UV_G,KF_SCALARS_G,KF_GP,KF_FS,KF_OUT_LT,&
+ & KF_UV,KF_SCALARS,KF_SCDERS,&
+ & YLGVU,YLGVV,&
+ & YLGVVOR,YLGVDIV,&
+ & YLGVSCALAR,&
+ & YLGVU_EW,YLGVV_EW,&
+ & YLGVSCALAR_NS, YLGVSCALAR_EW,&
+ & FSPGL_PROC)
+
+!**** *INV_TRANS_VIEW_CTL* - Control routine for inverse spectral transform.
+
+!     Purpose.
+!     --------
+!        Control routine for the inverse spectral transform
+
+!**   Interface.
+!     ----------
+!     CALL INV_TRANS_VIEW_CTL(...)
+
+!     Explicit arguments :
+!     --------------------
+!     KF_UV_G      - global number of spectral u-v fields
+!     KF_SCALARS_G - global number of scalar spectral fields
+!     KF_GP        - total number of output gridpoint fields
+!     KF_FS        - total number of fields in fourier space
+!     KF_OUT_LT    - total number of fields coming out from inverse LT
+!     KF_UV        - local number of spectral u-v fields
+!     KF_SCALARS   - local number of scalar spectral fields
+!     KF_SCDERS    - local number of derivatives of scalar spectral fields
+!     PSPVOR(:,:)  - spectral vorticity (input)
+!     PSPDIV(:,:)  - spectral divergence (input)
+!     PSPSCALAR(:,:) - spectral scalarvalued fields (input)
+!     KVSETUV(:)  - indicating which 'b-set' in spectral space owns a
+!                   vor/div field. Equivalant to NBSETLEV in the IFS.
+!                   The length of KVSETUV should be the GLOBAL number
+!                   of u/v fields which is the dimension of u and v releated
+!                   fields in grid-point space.
+!     KVESETSC(:) - indicating which 'b-set' in spectral space owns a
+!                   scalar field. As for KVSETUV this argument is required
+!                   if the total number of processors is greater than
+!                   the number of processors used for distribution in
+!                   spectral wave space.
+!     FSPGL_PROC  - external procedure to be executed in fourier space
+!                   before transposition
+!     PGP(:,:,:)  - gridpoint fields (output)
+
+!                  The ordering of the output fields is as follows (all
+!                  parts are optional depending on the input switches):
+
+!       vorticity     : KF_UV_G fields
+!       divergence    : KF_UV_G fields
+!       u             : KF_UV_G fields
+!       v             : KF_UV_G fields
+!       scalar fields : KF_SCALARS_G fields
+!       N-S derivative of scalar fields : KF_SCALARS_G fields
+!       E-W derivative of u : KF_UV_G fields
+!       E-W derivative of v : KF_UV_G fields
+!       E-W derivative of scalar fields : KF_SCALARS_G fields
+
+!     Method.
+!     -------
+
+!     Externals.  SHUFFLE     - reshuffle fields for load balancing
+!     ----------  FIELD_SPLIT - split fields in NPROMATR packets
+!                 LTINV_CTL   - control of Legendre transform
+!                 FTINV_CTL   - control of Fourier transform
+
+!     Author.
+!     -------
+!        Mats Hamrud *ECMWF*
+
+!     Modifications.
+!     --------------
+!        Original : 01-01-03
+
+!     ------------------------------------------------------------------
+
+
+USE PARKIND1  ,ONLY : JPIM     ,JPRB
+
+USE TPM_GEN         ,ONLY : NPROMATR
+USE TPM_TRANS       ,ONLY : LDIVGP, LSCDERS, LUVDER, LVORGP
+
+USE SHUFFLE_MOD     ,ONLY : SHUFFLE
+USE FIELD_SPLIT_MOD ,ONLY : FIELD_SPLIT
+USE LTINV_VIEW_CTL_MOD   ,ONLY : LTINV_VIEW_CTL
+USE FTINV_VIEW_CTL_MOD   ,ONLY : FTINV_VIEW_CTL
+!SE ECTRANS_FIELD_VIEW_INTERNAL_UTIL_MOD, ONLY : SPEC_VIEW, GRID_VIEW, LS_COUNT, LG_COUNT, LS, LG, IVSET_PTR, &
+ !                                             & GET_NPROMA, GET_NFLD, GET_NSPEC2, GET_NBLK
+!
+
+IMPLICIT NONE
+
+! Declaration of arguments
+
+INTEGER(KIND=JPIM), INTENT(IN) :: KF_UV_G
+INTEGER(KIND=JPIM), INTENT(IN) :: KF_SCALARS_G
+INTEGER(KIND=JPIM), INTENT(IN) :: KF_GP
+INTEGER(KIND=JPIM), INTENT(IN) :: KF_FS
+INTEGER(KIND=JPIM), INTENT(IN) :: KF_OUT_LT
+INTEGER(KIND=JPIM), INTENT(IN) :: KF_UV
+INTEGER(KIND=JPIM), INTENT(IN) :: KF_SCALARS
+INTEGER(KIND=JPIM), INTENT(IN) :: KF_SCDERS
+
+TYPE(SPEC_VIEW), ALLOCATABLE :: YLSPVVOR(:), YLSPVDIV(:)
+TYPE(SPEC_VIEW), ALLOCATABLE :: YLSPVSCALAR(:)
+
+TYPE(GRID_VIEW), ALLOCATABLE :: YLGVU(:),YLGVV(:)
+TYPE(GRID_VIEW), ALLOCATABLE :: YLGVVOR(:),YLGVDIV(:)
+TYPE(GRID_VIEW), ALLOCATABLE :: YLGVSCALAR(:)
+
+TYPE(GRID_VIEW), ALLOCATABLE :: YLGVU_EW(:),YLGVV_EW(:)
+TYPE(GRID_VIEW), ALLOCATABLE :: YLGVSCALAR_NS(:), YLGVSCALAR_EW(:)
+
+EXTERNAL  FSPGL_PROC
+OPTIONAL  FSPGL_PROC
+
+
+! Local variables
+
+INTEGER(KIND=JPIM) :: IPTRGP(KF_GP),IPTRSPUV(NPROMATR),IPTRSPSC(NPROMATR)
+INTEGER(KIND=JPIM) :: ISHFUV_G(KF_GP),ISHFSC_G(KF_GP)
+INTEGER(KIND=JPIM) :: IVSETUV(KF_GP),IVSETSC(KF_GP)
+INTEGER(KIND=JPIM) :: IBLKS,JBLK,ISTUV_G,IENUV_G
+INTEGER(KIND=JPIM) :: IF_UV_G,IF_UV,ISTUV,IF_SCALARS,IF_SCALARS_G,IF_FS,IF_GP
+INTEGER(KIND=JPIM) :: JFLD,ISTSC_G,IENSC_G,ISTSC,IENSC,IENUV,IF_SCDERS,IF_OUT_LT
+INTEGER(KIND=JPIM) :: IOFFD,IOFFU,IOFFV,IOFFUVD,IOFFSC,IOFFSCNS,IOFFSCEW,IOFF,IF_GPB
+
+!     ------------------------------------------------------------------
+
+! Perform transform
+
+IF_GPB = 2*KF_UV_G+KF_SCALARS_G
+IF(NPROMATR > 0 .AND. IF_GPB > NPROMATR) THEN
+
+  CALL ABOR1('INV_TRANS_VIEW NPROMATR not implemented')
+
+ELSE
+
+  ! No splitting of fields, transform done in one go
+  CALL LTINV_VIEW_CTL(KF_OUT_LT,KF_UV,KF_SCALARS,KF_SCDERS, &
+   &YLSPVVOR=YLSPVVOR,YLSPVDIV=YLSPVDIV,YLSPVSCALAR=YLSPVSCALAR,&
+   &FSPGL_PROC=FSPGL_PROC)
+
+  CALL FTINV_VIEW_CTL(KF_UV_G,KF_SCALARS_G,&
+   & KF_UV,KF_SCALARS,KF_SCDERS,KF_GP,KF_FS,KF_OUT_LT,&
+   & YLGVU=YLGVU,YLGVV=YLGVV,YLGVSCALAR=YLGVSCALAR)
+
+ENDIF
+
+!     ------------------------------------------------------------------
+
+END SUBROUTINE INV_TRANS_VIEW_CTL
+END MODULE INV_TRANS_VIEW_CTL_MOD
---
+++ src/trans/cpu/internal/trltog_view_mod.F90
@@ -0,0 +1,342 @@
+! (C) Copyright 1995- ECMWF.
+! (C) Copyright 1995- Meteo-France.
+!
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+!
+
+MODULE TRLTOG_VIEW_MOD
+
+IMPLICIT NONE
+
+PUBLIC TRLTOG_VIEW
+PRIVATE TRLTOG_VIEW_COMM
+
+CONTAINS
+
+SUBROUTINE TRLTOG_VIEW(PGLAT, KF_FS, KF_GP, KF_SCALARS_G, KVSET, KPTRGP, YLGVU,YLGVV,YLGVSCALAR)
+
+!**** *TRLTOG_VIEW * - head routine for transposition of grid point data from latitudinal
+!                 to column structure (this takes place between inverse
+!                 FFT and grid point calculations)
+!                 TRLTOG_VIEW is the inverse of TRGTOL
+
+!**   Interface.
+!     ----------
+!        *call* *TRLTOG_VIEW(...)
+
+!        Explicit arguments :
+!        --------------------
+!           PGLAT    -  Latitudinal data ready for direct FFT (input)
+!           PGP    -  Blocked grid point data    (output)
+!           KVSET    - "v-set" for each field      (input)
+
+!        Implicit arguments :
+!        --------------------
+
+!     Method.
+!     -------
+!        See documentation
+
+!     Externals.
+!     ----------
+
+!     Reference.
+!     ----------
+!        ECMWF Research Department documentation of the IFS
+
+!     Author.
+!     -------
+!        R. El Khatib *Meteo-France*
+
+!     Modifications.
+!     --------------
+!        Original  : 18-Aug-2014 from TRLTOG_VIEW
+!        R. El Khatib 09-Sep-2020 NSTACK_MEMORY_TR
+!     ------------------------------------------------------------------
+
+USE PARKIND1  ,ONLY : JPIM     ,JPRB
+USE YOMHOOK   ,ONLY : LHOOK,   DR_HOOK, JPHOOK
+
+USE TPM_DISTR       ,ONLY : D
+USE TRGL_MOD, ONLY: TRGL_BUFFERS, ALLOCATE_BUFFERS_CST, TRGL_PROLOG, ALLOCATE_BUFFERS_SR
+USE ECTRANS_FIELD_VIEW_MOD, ONLY : GRID_VIEW
+
+IMPLICIT NONE
+
+REAL(KIND=JPRB),INTENT(IN)     :: PGLAT(KF_FS,D%NLENGTF)
+INTEGER(KIND=JPIM),INTENT(IN)  :: KF_FS,KF_GP
+INTEGER(KIND=JPIM),INTENT(IN)  :: KF_SCALARS_G
+INTEGER(KIND=JPIM), INTENT(IN) :: KVSET(KF_GP)
+INTEGER(KIND=JPIM) ,OPTIONAL, INTENT(IN) :: KPTRGP(:)
+
+TYPE(GRID_VIEW), INTENT(OUT), ALLOCATABLE :: YLGVU(:),YLGVV(:)
+TYPE(GRID_VIEW), INTENT(OUT), ALLOCATABLE :: YLGVSCALAR(:)
+
+TYPE(TRGL_BUFFERS) :: YDBUFS
+
+REAL(KIND=JPHOOK) :: ZHOOK_HANDLE
+
+!     ------------------------------------------------------------------
+
+IF (LHOOK) CALL DR_HOOK('TRLTOG_VIEW',0,ZHOOK_HANDLE)
+
+YDBUFS%LLTRGTOL = .FALSE.
+CALL ALLOCATE_BUFFERS_CST(YDBUFS)
+CALL GSTATS(1806, 0)
+CALL TRGL_PROLOG(KF_FS, KF_GP, KVSET, YDBUFS)
+CALL GSTATS(1806, 1)
+CALL ALLOCATE_BUFFERS_SR(YDBUFS, KF_GP)
+
+CALL TRLTOG_VIEW_COMM(PGLAT, KF_FS, KF_GP, KF_SCALARS_G, KVSET, KPTRGP, YLGVU,YLGVV,YLGVSCALAR, &
+  &              YDBUFS)
+
+IF (LHOOK) CALL DR_HOOK('TRLTOG_VIEW',1,ZHOOK_HANDLE)
+
+!     ------------------------------------------------------------------
+
+END SUBROUTINE TRLTOG_VIEW
+
+SUBROUTINE TRLTOG_VIEW_COMM(PGLAT, KF_FS, KF_GP, KF_SCALARS_G, KVSET, KPTRGP, YLGVU,YLGVV,YLGVSCALAR,YDBUFS)
+
+
+!**** *TRLTOG_VIEW * - transposition of grid point data from latitudinal
+!                 to column structure. This takes place between inverse
+!                 FFT and grid point calculations.
+!                 TRLTOG_VIEW_COMM is the inverse of TRGTOL
+
+!     Purpose.
+!     --------
+
+!**   Interface.
+!     ----------
+!        *call* *TRLTOG_VIEW(...)
+
+!        Explicit arguments :
+!        --------------------
+!           PGLAT    -  Latitudinal data ready for direct FFT (input)
+!           PGP    -  Blocked grid point data    (output)
+!           KVSET    - "v-set" for each field      (input)
+
+!        Implicit arguments :
+!        --------------------
+
+!     Method.
+!     -------
+!        See documentation
+
+!     Externals.
+!     ----------
+
+!     Reference.
+!     ----------
+!        ECMWF Research Department documentation of the IFS
+
+!     Author.
+!     -------
+!        MPP Group *ECMWF*
+
+!     Modifications.
+!     --------------
+!        Original  : 95-10-01
+!        D.Dent    : 97-08-04 Reorganisation to allow NPRTRV
+!                             to differ from NPRGPEW
+!        =99-03-29= Mats Hamrud and Deborah Salmond
+!                   JUMP in FFT's changed to 1
+!                   KINDEX introduced and PCOMBUF not used for same PE
+!         01-11-23  Deborah Salmond and John Hague
+!                   LIMP_NOOLAP Option for non-overlapping message passing
+!                               and buffer packing
+!         01-12-18  Peter Towers
+!                   Improved vector performance of LTOG_PACK,LTOG_UNPACK
+!         03-0-02   G. Radnoti: Call barrier always when nproc>1
+!         08-01-01  G.Mozdzynski: cleanup
+!         09-01-02  G.Mozdzynski: use non-blocking recv and send
+!        R. El Khatib 09-Sep-2020 64 bits addressing for PGLAT
+!     ------------------------------------------------------------------
+
+USE PARKIND1  ,ONLY : JPIM     ,JPRB
+USE YOMHOOK   ,ONLY : LHOOK,   DR_HOOK, JPHOOK
+
+USE MPL_MODULE  ,ONLY : MPL_RECV, MPL_SEND, MPL_WAIT, JP_NON_BLOCKING_STANDARD, MPL_WAITANY, &
+  &                     JP_BLOCKING_STANDARD, MPL_BARRIER, JP_BLOCKING_BUFFERED
+
+USE TPM_GEN         ,ONLY : NTRANS_SYNC_LEVEL, NSTACK_MEMORY_TR
+USE TPM_DISTR       ,ONLY : D, MTAGLG, NPRCIDS, MYPROC, NPROC
+
+USE TRGL_MOD, ONLY: TRGL_BUFFERS, TRGL_VARS, TRGL_ALLOCATE_VARS, TRGL_ALLOCATE_HEAP_BUFFER, &
+  &                 TRGL_INIT_VARS, TRGL_INIT_OFF_VARS, TGRL_COPY_ZCOMBUF, TGRL_COPY_PGLAT, &
+  &                 TGRL_INIT_PACKING_VARS
+
+IMPLICIT NONE
+
+INTEGER(KIND=JPIM), INTENT(IN) :: KF_FS,KF_GP
+REAL(KIND=JPRB),INTENT(IN)     :: PGLAT(KF_FS,D%NLENGTF)
+INTEGER(KIND=JPIM), INTENT(IN) :: KVSET(KF_GP)
+INTEGER(KIND=JPIM), INTENT(IN) :: KF_SCALARS_G
+INTEGER(KIND=JPIM) ,OPTIONAL, INTENT(IN) :: KPTRGP(:)
+
+TYPE(GRID_VIEW), INTENT(OUT), ALLOCATABLE :: YLGVU(:),YLGVV(:)
+TYPE(GRID_VIEW), INTENT(OUT), ALLOCATABLE :: YLGVSCALAR(:)
+TYPE (TRGL_BUFFERS), INTENT(INOUT), TARGET :: YDBUFS
+! LOCAL VARIABLES
+TYPE(TRGL_VARS) :: YLVARS
+
+INTEGER(KIND=JPIM) :: IREQ_SEND(NPROC)
+INTEGER(KIND=JPIM) :: IREQ_RECV(NPROC)
+
+!     LOCAL INTEGER SCALARS
+INTEGER(KIND=JPIM) :: IRECV
+INTEGER(KIND=JPIM) :: ISEND, ITAG, JL, JFLD, INS, INR, JNR
+INTEGER(KIND=JPIM) :: II,ILEN
+INTEGER(KIND=JPIM) :: ISEND_FLD_START,ISEND_FLD_END
+
+!     LOCAL ARRAYS
+REAL(KIND=JPRB), TARGET :: ZCOMBUFS_STACK(-1:YDBUFS%ISENDCOUNT,MERGE (YDBUFS%INSEND,0,NSTACK_MEMORY_TR/=0))
+REAL(KIND=JPRB), TARGET :: ZCOMBUFR_STACK(-1:YDBUFS%IRECVCOUNT,MERGE (YDBUFS%INRECV,0,NSTACK_MEMORY_TR/=0))
+
+REAL(KIND=JPRB), ALLOCATABLE, TARGET, SAVE :: ZCOMBUFS_HEAP(:,:)
+REAL(KIND=JPRB), ALLOCATABLE, TARGET, SAVE :: ZCOMBUFR_HEAP(:,:)
+
+REAL(KIND=JPRB), POINTER, CONTIGUOUS :: ZCOMBUFS(:,:)
+REAL(KIND=JPRB), POINTER, CONTIGUOUS :: ZCOMBUFR(:,:)
+
+REAL(KIND=JPHOOK) :: ZHOOK_HANDLE_BAR
+
+!     ------------------------------------------------------------------
+
+!*       0.    Some initializations
+!              --------------------
+ASSOCIATE(KNSEND=>YDBUFS%INSEND, KNRECV=>YDBUFS%INRECV, KSENDTOT=>YDBUFS%ISENDTOT, &
+  &       KRECVTOT=>YDBUFS%IRECVTOT, KSEND=>YDBUFS%ISEND, KRECV=>YDBUFS%IRECV, &
+  &       KINDEX=>YDBUFS%IINDEX, KNDOFF=>YDBUFS%INDOFF)
+
+IF (NSTACK_MEMORY_TR == 0) THEN
+  CALL TRGL_ALLOCATE_HEAP_BUFFER(ZCOMBUFS_HEAP, YDBUFS%ISENDCOUNT, YDBUFS%INSEND)
+  CALL TRGL_ALLOCATE_HEAP_BUFFER(ZCOMBUFR_HEAP, YDBUFS%IRECVCOUNT, YDBUFS%INRECV)
+
+! Now, force the OS to allocate this shared array right now, not when it starts to be used which is
+! an OPEN-MP loop, that would cause a threads synchronization lock :
+  IF (YDBUFS%INSEND > 0 .AND. YDBUFS%ISENDCOUNT >=-1) ZCOMBUFS_HEAP(-1,1)=HUGE(1._JPRB)
+  ZCOMBUFS (-1:,1:) => ZCOMBUFS_HEAP
+  ZCOMBUFR (-1:,1:) => ZCOMBUFR_HEAP
+ELSE
+  ZCOMBUFS (-1:,1:) => ZCOMBUFS_STACK
+  ZCOMBUFR (-1:,1:) => ZCOMBUFR_STACK
+ENDIF
+
+ITAG = MTAGLG
+
+IF (LHOOK) CALL DR_HOOK('TRLTOG_VIEW_BAR',0,ZHOOK_HANDLE_BAR)
+CALL GSTATS_BARRIER(762)
+IF (LHOOK) CALL DR_HOOK('TRLTOG_VIEW_BAR',1,ZHOOK_HANDLE_BAR)
+
+CALL GSTATS(805,0)
+
+IF (NTRANS_SYNC_LEVEL <= 0) THEN
+  !...Receive loop.........................................................
+  DO INR=1,KNRECV
+    IRECV=KRECV(INR)
+    CALL MPL_RECV(ZCOMBUFR(-1:KRECVTOT(IRECV),INR), KSOURCE=NPRCIDS(IRECV), &
+      &           KMP_TYPE=JP_NON_BLOCKING_STANDARD, KREQUEST=IREQ_RECV(INR), KTAG=ITAG, &
+      &           CDSTRING='TRLTOG_VIEW_COMM: NON-BLOCKING IRECV' )
+  ENDDO
+ENDIF
+
+CALL GSTATS(805,1)
+
+#if 0
+CALL GSTATS(1806,0)
+YDBUFS%LLINDER = PRESENT(KPTRGP)
+YDBUFS%LLPGPONLY = PRESENT(PGP)
+CALL TRGL_ALLOCATE_VARS(YLVARS, KF_GP,KF_FS)
+CALL TRGL_INIT_VARS(YLVARS, KF_SCALARS_G, PGP, PGPUV, PGP3A, PGP3B, PGP2)
+CALL GSTATS(1806,1)
+#endif
+! Copy local contribution
+
+#if 0
+IF(KRECVTOT(MYPROC) > 0 )THEN
+  CALL TRGL_INIT_OFF_VARS(YDBUFS,YLVARS,KVSET,KPTRGP,KF_GP)
+  CALL GSTATS(1604,0)
+  CALL TGRL_COPY_PGLAT(PGLAT, YDBUFS, YLVARS, PGP, PGPUV,PGP3A, PGP3B,PGP2)
+  CALL GSTATS(1604,1)
+ENDIF
+#endif
+! loop over the number of processors we need to communicate with.
+! NOT MYPROC
+!
+! Now overlapping buffer packing/unpacking with sends/waits
+! Time as if all communications to avoid double accounting
+
+CALL GSTATS(805,0)
+
+!  Pack+send loop.........................................................
+
+ISEND_FLD_START = 1
+ISEND_FLD_END   = KF_FS
+DO INS=1,KNSEND
+  ISEND=KSEND(INS)
+  ILEN = KSENDTOT(ISEND)/KF_FS
+  !$OMP PARALLEL DO SCHEDULE(STATIC) PRIVATE(JFLD,JL,II)
+  DO JL=1,ILEN
+    II = KINDEX(KNDOFF(ISEND)+JL)
+    DO JFLD=ISEND_FLD_START,ISEND_FLD_END
+      ZCOMBUFS((JFLD-ISEND_FLD_START)*ILEN+JL,INS) = PGLAT(JFLD,II)
+    ENDDO
+  ENDDO
+  !$OMP END PARALLEL DO
+  ZCOMBUFS(-1,INS) = 1
+  ZCOMBUFS(0,INS)  = KF_FS
+  IF (NTRANS_SYNC_LEVEL <= 1) THEN
+    CALL MPL_SEND(ZCOMBUFS(-1:KSENDTOT(ISEND),INS), KDEST=NPRCIDS(ISEND), &
+      &           KMP_TYPE=JP_NON_BLOCKING_STANDARD, KREQUEST=IREQ_SEND(INS), KTAG=ITAG, &
+      &           CDSTRING='TRLTOG_VIEW_COMM: NON-BLOCKING ISEND')
+  ELSE
+    CALL MPL_SEND(ZCOMBUFS(-1:KSENDTOT(ISEND),INS), KDEST=NPRCIDS(ISEND), &
+      &           KMP_TYPE=JP_BLOCKING_BUFFERED, KTAG=ITAG, &
+      &           CDSTRING='TRLTOG_VIEW_COMM: BLOCKING BUFFERED BSEND')
+  ENDIF
+ENDDO
+
+!  Unpack loop.........................................................
+
+CALL TGRL_INIT_PACKING_VARS(YDBUFS,YLVARS, KVSET, KF_GP)
+
+DO JNR=1,KNRECV
+
+  IF (NTRANS_SYNC_LEVEL <= 0) THEN
+    CALL MPL_WAITANY(KREQUEST=IREQ_RECV(1:KNRECV), KINDEX=INR, &
+      &              CDSTRING='TRLTOG_VIEW_COMM: WAIT FOR ANY RECEIVES')
+  ELSE
+    INR = JNR
+    IRECV=KRECV(INR)
+    CALL MPL_RECV(ZCOMBUFR(-1:KRECVTOT(IRECV),INR), KSOURCE=NPRCIDS(IRECV), &
+          & KMP_TYPE=JP_BLOCKING_STANDARD, KTAG=ITAG, CDSTRING='TRLTOG_VIEW_COMM: BLOCKING RECV')
+  ENDIF
+#if 0
+  CALL TGRL_COPY_ZCOMBUF(YDBUFS, YLVARS, INR, ZCOMBUFR, KPTRGP, PGP, PGPUV, PGP3A, PGP3B, PGP2)
+#endif
+ENDDO
+
+IF (NTRANS_SYNC_LEVEL <= 1) THEN
+  IF(KNSEND > 0) THEN
+    CALL MPL_WAIT(KREQUEST=IREQ_SEND(1:KNSEND),CDSTRING='TRLTOG_VIEW_COMM: WAIT FOR ISENDS')
+  ENDIF
+ENDIF
+
+IF (NTRANS_SYNC_LEVEL >= 1) THEN
+  CALL MPL_BARRIER(CDSTRING='TRLTOG_VIEW_COMM: BARRIER AT END')
+ENDIF
+
+CALL GSTATS(805,1)
+
+CALL GSTATS_BARRIER2(762)
+
+END ASSOCIATE
+
+END SUBROUTINE TRLTOG_VIEW_COMM
+END MODULE TRLTOG_VIEW_MOD
