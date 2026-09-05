008c0d98dabbad1ec069c23d83353d405cd594dc
Denis Haumont <76622294+dhaumont@users.noreply.github.com>
2026-09-05 09:59:42 +0200

    zz

---
+++ meld.sh
@@ -0,0 +1,26 @@
+meld ./src/trans/cpu/internal/dir_trans_ctl_mod.F90 src/trans/field_view/cpu/internal/dir_trans_view_ctl_mod.F90
+meld ./src/trans/cpu/internal/fsc_mod.F90 src/trans/field_view/cpu/internal/fsc_view_mod.F90
+meld ./src/trans/cpu/internal/ftdir_ctl_mod.F90 src/trans/field_view/cpu/internal/ftdir_view_ctl_mod.F90
+meld ./src/trans/cpu/internal/ftinv_ctl_mod.F90 src/trans/field_view/cpu/internal/ftinv_view_ctl_mod.F90
+meld ./src/trans/cpu/internal/inv_trans_ctl_mod.F90 src/trans/field_view/cpu/internal/inv_trans_view_ctl_mod.F90
+meld ./src/trans/cpu/internal/ltdir_ctl_mod.F90 src/trans/field_view/cpu/internal/ltdir_view_ctl_mod.F90
+meld ./src/trans/cpu/internal/ltdir_mod.F90 src/trans/field_view/cpu/internal/ltdir_view_mod.F90
+meld ./src/trans/cpu/internal/ltinv_ctl_mod.F90 src/trans/field_view/cpu/internal/ltinv_view_ctl_mod.F90
+meld ./src/trans/cpu/internal/ltinv_mod.F90 src/trans/field_view/cpu/internal/ltinv_view_mod.F90
+meld ./src/trans/cpu/internal/prfi1b_mod.F90 src/trans/field_view/cpu/internal/prfi1b_view_mod.F90
+meld ./src/trans/cpu/internal/trgl_mod.F90 src/trans/field_view/cpu/internal/trgl_view_mod.F90
+meld ./src/trans/cpu/internal/trgtol_mod.F90 src/trans/field_view/cpu/internal/trgtol_view_mod.F90
+meld ./src/trans/cpu/internal/trltog_mod.F90 src/trans/field_view/cpu/internal/trltog_view_mod.F90
+meld ./src/trans/cpu/internal/updspb_mod.F90 src/trans/field_view/cpu/internal/updspb_view_mod.F90
+meld ./src/trans/cpu/internal/updsp_mod.F90 src/trans/field_view/cpu/internal/updsp_view_mod.F90
+
+
+meld ./src/trans/gpu/internal/dir_trans_ctl_mod.F90 src/trans/field_view/gpu/internal/dir_trans_view_ctl_mod.F90
+meld ./src/trans/gpu/internal/inv_trans_ctl_mod.F90 src/trans/field_view/gpu/internal/inv_trans_view_ctl_mod.F90
+meld ./src/trans/gpu/internal/ltdir_mod.F90 src/trans/field_view/gpu/internal/ltdir_view_mod.F90
+meld ./src/trans/gpu/internal/ltinv_mod.F90 src/trans/field_view/gpu/internal/ltinv_view_mod.F90
+meld ./src/trans/gpu/internal/prfi1b_mod.F90 src/trans/field_view/gpu/internal/prfi1b_view_mod.F90
+meld ./src/trans/gpu/internal/trgtol_mod.F90 src/trans/field_view/gpu/internal/trgtol_view_mod.F90
+meld ./src/trans/gpu/internal/trltog_mod.F90 src/trans/field_view/gpu/internal/trltog_view_mod.F90
+meld ./src/trans/gpu/internal/updspb_mod.F90 src/trans/field_view/gpu/internal/updspb_view_mod.F90
+meld ./src/trans/gpu/internal/updsp_mod.F90 src/trans/field_view/gpu/internal/updsp_view_mod.F90
